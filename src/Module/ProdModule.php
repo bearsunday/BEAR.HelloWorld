14424619b09bc7662058b995dd1b4680be062774
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-03-11 02:42:32 +0900

    プロダクションを追加

--- src/Module/ProdModule.php
+++ src/Module/ProdModule.php
@@ -1,21 +1,14 @@
 <?php
-/**
- * This file is part of the BEAR.Package package
- *
- * @license http://opensource.org/licenses/bsd-license.php BSD
- */
+
 namespace BEAR\HelloWorld\Module;
 
 use BEAR\RepositoryModule\Annotation\Storage;
-use BEAR\Resource\Annotation\LogicCache;
-use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Annotations\CachedReader;
-use Doctrine\Common\Annotations\Reader;
-use Doctrine\Common\Cache\ApcCache;
+use BEAR\Package\Context\ProdModule as PackageProdModule;
 use Doctrine\Common\Cache\Cache;
-use Doctrine\Common\Cache\CacheProvider;
 use Ray\Di\AbstractModule;
 use Ray\Di\Scope;
+
+use Doctrine\Common\Cache\ApcCache;
 
 class ProdModule extends AbstractModule
 {
@@ -24,14 +17,10 @@ class ProdModule extends AbstractModule
      */
     protected function configure()
     {
-        $this->bind(Cache::class)->to(ApcCache::class)->in(Scope::SINGLETON);
-        $this->bind(Reader::class)->toConstructor(
-            CachedReader::class,
-            'reader=annotation_reader'
-        );
-        $this->bind(Reader::class)->annotatedWith('annotation_reader')->to(AnnotationReader::class);
-        $this->bind(CacheProvider::class)->annotatedWith(Storage::class)->to(ApcCache::class)->in(Scope::SINGLETON);
-        $this->bind(Cache::class)->annotatedWith(LogicCache::class)->to(ApcCache::class)->in(Scope::SINGLETON);
-        $this->bind(Cache::class)->annotatedWith(Storage::class)->to(ApcCache::class)->in(Scope::SINGLETON);
+        // configure shared storage for query repository
+        $cache = ApcCache::class;
+        $this->bind(Cache::class)->annotatedWith(Storage::class)->to($cache)->in(Scope::SINGLETON);
+
+        $this->install(new PackageProdModule);
     }
 }
